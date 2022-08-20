f39471e6896774743145c56cc6930c684979e157
Chris Farber <chris@chrisfarber.net>
2022-08-20 16:04:36 -0400

    stuff

--- src/mutability.rs
+++ src/mutability.rs
@@ -13,10 +13,10 @@ fn largest_element_of<'a, T: PartialOrd>(v: &'a mut Vec<T>) -> Option<&'a mut T>
 }
 
 pub fn try_crazy_stuff() {
-    let mut stuff = vec![1, 2, 3, 5, 3, 2, 1];
+    let mut stuff = vec![1, 2, 3, 5, 27, 3, 2, 1];
     let wat = largest_element_of(&mut stuff);
-    if let Some(numRef) = wat {
-        *numRef = 42;
+    if let Some(num) = wat {
+        *num = 42;
     }
 
     println!("stuff? {:?}", stuff);
