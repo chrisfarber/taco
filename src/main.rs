92024d98749adc280e49d9cb4a45f901b4e2bb58
Chris Farber <chris@chrisfarber.net>
2022-08-20 08:41:09 -0400

    playing around with web server stuff

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,49 @@
-use std::io::{self, Write};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
 mod parser;
 
-fn main() {
-    let mut text = String::new();
+async fn answer(req: Request<Body>) -> Result<Response<Body>, hyper::http::Error> {
+    Response::builder()
+        .status(200)
+        .header("woah", "dude\r\n")
+        .body(req.into_body())
+}
 
-    print!("Text: ");
-    io::stdout().flush().expect("how could this fail?");
+async fn echo_service(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    error_to_500(answer(req).await)
+}
 
-    io::stdin()
-        .read_line(&mut text)
-        .expect("should have read the line dude.");
-
-    let wat = parser::parens(&text);
-
-    match wat {
-        Ok((a, b)) => {
-            println!("first? '{a}'");
-            println!("second? '{b}'");
-        }
-        Err(e) => {
-            println!("nope. {e}");
-        }
+fn error_to_500<E>(result: Result<Response<Body>, E>) -> Result<Response<Body>, Infallible> {
+    match result {
+        Ok(resp) => Ok(resp),
+        Err(_) => Ok(Response::builder()
+            .status(500)
+            .body("oh no".into())
+            .unwrap()),
     }
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+#[tokio::main]
+async fn main() {
+    parser::stuff();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(echo_service)) });
+
+    let server = Server::bind(&addr).serve(make_svc);
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    println!("listening.");
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+    println!("bye");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,41 @@ use nom::{
     sequence::delimited,
     IResult,
 };
+use std::collections::HashMap;
 
 pub fn parens(input: &str) -> IResult<&str, &str> {
     delimited(char('('), is_not(")"), char(')'))(input)
+}
+
+fn wat() -> HashMap<String, i32> {
+    let mut map = HashMap::new();
+    map.insert("foo".into(), 42);
+    return map;
+}
+
+pub fn stuff() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let huh = scores.entry("Fancy".into());
+    let wtf = huh.or_insert(42);
+    *wtf += 1;
+
+    println!("later {:?}", scores);
+}
+
+fn err() {
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    // println!("{field_name} {field_value}");
 }
