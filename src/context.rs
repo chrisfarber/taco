593341f524e6f68afef2eb6a042806e2e30e2f7c
Chris Farber <chris@chrisfarber.net>
2022-08-27 11:56:22 -0400

    switch over to axum

--- src/context.rs
+++ src/context.rs
@@ -1,9 +1,1 @@
-pub struct Context {
-    state: i32,
-}
 
-impl Context {
-    pub fn new(state: i32) -> Self {
-        Self { state: state }
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,11 @@
-use hyper::{Body, Request, Server};
-use routerify::{Middleware, Router, RouterService};
-use std::convert::Infallible;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+
 use std::net::SocketAddr;
 
 mod context;
 mod handler;
-
-// async fn main_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-//     let ctx = context::Context { state: 4 };
-//     let handler1: Box<&dyn handler::Handler> = Box::new(&handler::hello);
-//     let handler: Box<dyn handler::Handler> = match (req.method(), req.uri().path()) {
-//         (&Method::GET, "/hello") => Box::new(handler::hello),
-//         (_, _) => Box::new(handler::not_found),
-//     };
-//     return error_to_500(handler.invoke(&ctx, &req).await);
-// }
-
-// fn error_to_500<E>(result: Result<Response<Body>, E>) -> Result<Response<Body>, Infallible> {
-//     match result {
-//         Ok(resp) => Ok(resp),
-//         Err(_) => Ok(Response::builder()
-//             .status(500)
-//             .body("oh no".into())
-//             .unwrap()),
-//     }
-// }
-
-// A middleware which logs an http request.
-async fn logger(req: Request<Body>) -> Result<Request<Body>, Infallible> {
-    println!(
-        "{} {} {}",
-        // req.remote_addr(),
-        "eh",
-        req.method(),
-        req.uri().path()
-    );
-    Ok(req)
-}
-
-fn router() -> Router<Body, Infallible> {
-    // Create a router and specify the logger middleware and the handlers.
-    // Here, "Middleware::pre" means we're adding a pre middleware which will be executed
-    // before any route handlers.
-    Router::builder()
-        // Specify the state data which will be available to every route handlers,
-        // error handler and middlewares.
-        .data(context::Context::new(42))
-        .middleware(Middleware::pre(logger))
-        .get("/hello", handler::hello)
-        // .err_handler_with_info(error_handler)
-        .build()
-        .unwrap()
-}
 
 async fn shutdown_signal() {
     // Wait for the CTRL+C signal
@@ -59,26 +13,22 @@ async fn shutdown_signal() {
         .await
         .expect("failed to install CTRL+C signal handler");
 }
-#[tokio::main]
-async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let service = RouterService::new(router()).unwrap();
-    let server = Server::bind(&addr).serve(service);
-    let graceful = server.with_graceful_shutdown(shutdown_signal());
-
-    println!("listening.");
-    if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
-    }
-    println!("bye");
+async fn hello() -> impl IntoResponse {
+    (axum::http::status::StatusCode::OK, "Well this is easier.\n")
 }
 
-#[cfg(test)]
-mod tests {
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello));
+    // let service = ServiceBuilder::new().service_fn(app);
 
-    #[test]
-    fn test_addition_fails() {
-        assert_eq!(1, 1);
-    }
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    println!("Starting up on http://{:?}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .expect("server error");
 }
