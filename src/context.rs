af046cda1513a67a9ecc5ce0dcc6da90f72cad44
Chris Farber <chris@chrisfarber.net>
2022-08-28 20:35:06 -0400

    an in-memory kv store

--- src/context.rs
+++ src/context.rs
@@ -1,1 +1,20 @@
-
+/// This is a nonsense function
+///
+/// # But there's more
+///
+/// it so happens that you can call this function!
+///
+/// ```rust
+/// wat();
+/// ```
+///
+/// But what does it do? 🤷🏻‍♂️
+///
+pub fn wat() {
+    let stuff = vec![1, 2, 3];
+    let boxed = Box::new(stuff);
+    let v2 = boxed.iter().rev().map(|x| x + 1_i32).map(|n| n + 10_i32);
+    for item in v2 {
+        println!("found {}", item);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use axum::extract::Path;
 use axum::http::status::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
-use axum::{Json, Router, Server};
+use axum::{Extension, Json, Router, Server};
 use serde::{Deserialize, Serialize};
+use tokio::time::sleep;
 
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 mod context;
 mod handler;
@@ -16,8 +21,13 @@ async fn shutdown_signal() {
         .expect("failed to install CTRL+C signal handler");
 }
 
-async fn hello() -> impl IntoResponse {
-    (StatusCode::OK, "Well this is easy.\n")
+async fn hello(state: Extension<StateRef>) -> impl IntoResponse {
+    if let Ok(mut state) = state.lock() {
+        state.ctr += 1;
+        (StatusCode::OK, format!("Hello #{}!\n", state.ctr))
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "oh no.\n".into())
+    }
 }
 
 #[derive(Deserialize, Serialize)]
@@ -25,21 +35,78 @@ struct Echo {
     msg: String,
 }
 
+#[derive(Deserialize, Serialize)]
+struct KeyPair {
+    key: String,
+    value: Option<String>,
+}
+
 async fn json_echo(Json(data): Json<Echo>) -> Json<Echo> {
     println!("echo received \"{}\"", data.msg);
+    sleep(Duration::from_millis(500)).await;
     Json(data)
 }
 
+async fn get_key(
+    Path(key): Path<String>,
+    state: Extension<StateRef>,
+) -> Result<String, StatusCode> {
+    let value;
+    {
+        let store = &state
+            .lock()
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+            .store;
+        value = store.get(&key).ok_or(StatusCode::NOT_FOUND)?.clone()
+    }
+    Ok(value)
+}
+
+async fn set_key(
+    Path(key): Path<String>,
+    body: String,
+    state: Extension<StateRef>,
+) -> Result<(), StatusCode> {
+    {
+        state
+            .lock()
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+            .store
+            .insert(key, body);
+    }
+    Ok(())
+}
+
+struct State {
+    pub ctr: i32,
+    pub store: HashMap<String, String>,
+}
+
+impl State {
+    fn new() -> State {
+        State {
+            ctr: 0,
+            store: HashMap::new(),
+        }
+    }
+}
+
+type StateRef = Arc<Mutex<State>>;
+
 #[tokio::main]
 async fn main() {
+    let state_ref: StateRef = Arc::new(Mutex::new(State::new()));
+
+    context::wat();
     let app = Router::new()
         .route("/hello", get(hello))
         .route("/json-echo", post(json_echo))
-        .route("/kv/:key", get(hello));
+        .route("/kv/:key", get(get_key).post(set_key))
+        .layer(Extension(state_ref));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
-    println!("Starting up on http://{:?}", addr);
+    println!("Starting up on http://127.0.0.1:{}", addr.port());
     Server::bind(&addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
